05daf2d2ac1fc1e818dd8bf9cb1bcb45caf97091
mortenee <mortenee@gmail.com>
2019-08-19 14:29:05 +0200

    Windows dep installer

--- PWB.vbs
+++ PWB.vbs
@@ -6,32 +6,37 @@ wbpath = Left(WScript.ScriptFullName, Len(WScript.ScriptFullName) - Len(WScript.
 WshShell.CurrentDirectory = wbpath
 javaPath = wbpath & "jre\bin\javaw.exe"
 
-pyPathFile="tmp\pypath"
-Set objFile = FSO.CreateTextFile(pyPathFile,True)
-objFile.Write wbpath & "python\" 
-objFile.Close
-
-pwbPathFile="tmp\pwbpath" 
-Set objFile = FSO.CreateTextFile(pwbPathFile,True)
-objFile.Write wbpath & "PWB\"
-objFile.Close
+If Not FSO.FolderExists(wbpath & "/tmp") Then
+	Set objFolder = FSO.CreateFolder(wbpath & "/tmp")
+End If 
 
 configFile="tmp\pwb.ini"
-Set objFile = FSO.CreateTextFile(configFile,True)
-objFile.Write ""
+Set objFile = FSO.OpenTextFile(configFile, 2, True)
+objFile.WriteLine "[ENV]"
+objFile.WriteLine "py_path=" & wbpath & "python\" 
+objFile.WriteLine "os="
+objFile.WriteLine "pwb_path=" & wbpath & "PWB\"
 objFile.Close
 
 set args = WScript.Arguments
 jarpath = wbpath & "sqlworkbench.jar" 
-javaCmd = chr(34) & javaPath & chr(34) & " -jar " & chr(34) & jarpath & chr(34) & " -url=jdbc:h2:mem:PWB -password=""" & chr(34) & " -configDir=" & chr(34) & wbpath
 
+javaCmd = chr(34) & javaPath & chr(34) & " -jar " & chr(34) & jarpath & chr(34) & " -url=jdbc:h2:mem:PWB -password=""" & chr(34) & " -configDir=" & chr(34) & wbpath
 if (args.length > 0) then
-  for each arg in args
-    javaCmd = javaCmd & " " & arg
-  next
+	for each arg in args
+    	javaCmd = javaCmd & " " & arg
+  	next
 end if
 
-' WScript.Echo javaCmd
-retValue = WshShell.Run(javaCmd, 0, false)
-Set WshShell = Nothing
+If FSO.FileExists(jarpath) And FSO.FileExists(javaPath) Then
+	' WScript.Echo javaCmd
+	retValue = WshShell.Run(javaCmd, 0, false)
+	Set WshShell = Nothing
+else
+	Answer = _
+ 		Msgbox("Missing dependencies! Download now?", vbYesNo+vbCritical, "PWB Installer")
+	If Answer = vbYes Then
+		WshShell.run("powershell -executionpolicy bypass -noexit -file PWB/download_deps.ps1")
+	End If
+End If
 
---
+++ bin/PWB/download_deps.ps1
@@ -0,0 +1,61 @@
+function Get-RedirectedUrl
+{
+    Param (
+        [Parameter(Mandatory=$true)]
+        [String]$URL
+    )
+
+    $request = [System.Net.WebRequest]::Create($url)
+    $request.AllowAutoRedirect=$false
+    $response=$request.GetResponse()
+
+    If ($response.StatusCode -eq "Found")
+    {
+        $response.GetResponseHeader("Location")
+    }
+}
+
+[Net.ServicePointManager]::SecurityProtocol = "tls12, tls11, tls"
+$binPath = (get-item $PSScriptRoot).parent.FullName
+
+# Download SQL Workbench J
+$url= "https://www.sql-workbench.eu/Workbench-Build125.zip"
+$filename = [System.IO.Path]::GetFileName($url); 
+Write-Host "Downloading $filename (approx. 6MB)"
+Invoke-WebRequest -Uri $url -OutFile $filename
+Write-Host "Extracting $filename  to $binPath"
+Expand-Archive $filename -DestinationPath $binPath
+
+# Download Python
+$url= "https://www.python.org/ftp/python/3.6.8/python-3.6.8-embed-amd64.zip"
+$pythonPath = [IO.Path]::Combine($binPath, 'python')
+New-Item -ItemType Directory -Force -Path $pythonPath 
+$filename = [System.IO.Path]::GetFileName($url); 
+Write-Host "Downloading $filename (approx. 7MB)"
+Invoke-WebRequest -Uri $url -OutFile $filename
+Write-Host "Extracting $filename  to $binPath"
+Expand-Archive $filename -DestinationPath $pythonPath
+
+# Download wimlib
+$url= "https://wimlib.net/downloads/wimlib-1.13.1-windows-x86_64-bin.zip"
+$filename = [System.IO.Path]::GetFileName($url); 
+Write-Host "Downloading $filename (approx. 7MB)"
+Invoke-WebRequest -Uri $url -OutFile $filename
+Write-Host "Extracting $filename  to $binPath"
+Expand-Archive $filename -DestinationPath $PSScriptRoot
+
+#Download JRE
+$url= "https://api.adoptopenjdk.net/v2/binary/releases/openjdk11?openjdk_impl=hotspot&os=windows&arch=x64&release=latest&type=jre"
+$fUrl = Get-RedirectedUrl $url
+$filename = [System.IO.Path]::GetFileName($fUrl); 
+Write-Host "Downloading $filename (approx. 40MB)"
+Invoke-WebRequest -Uri $url -OutFile $filename
+Write-Host "Extracting JDK to $PSScriptRoot"
+Expand-Archive $filename -DestinationPath $binPath
+$jdkDir = Get-ChildItem $binPath | Where-Object {$_.name -like "jdk-*"} | Select-Object -First 1
+Rename-Item $jdkDir jre
+
+#Cleanup
+Get-ChildItem -Path $PSScriptRoot | ?{ $_.PSIsContainer } | foreach { Remove-Item -Path $_.FullName -Recurse -Force -Confirm:$false}
+Get-ChildItem -Path $PSScriptRoot\* -include *.txt,*.cmd | foreach { Remove-Item -Path $_.FullName }
+Get-ChildItem -Path $binPath\* -include *.ps1,*.cmd,*.sample,*.sh,*-sample.xml,*.vbs,*.exe,*.zip,*.pdf | foreach { Remove-Item -Path $_.FullName }
