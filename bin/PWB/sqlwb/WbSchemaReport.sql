6fbdefdce6727b45e19f980f4b72884a5fe58fa4
Morten Eek <mortenee@gmail.com>
2020-02-21 13:05:51 +0100

    Fixed wbschema path

--- bin/PWB/sqlwb/WbSchemaReport.sql
+++ bin/PWB/sqlwb/WbSchemaReport.sql
@@ -1,7 +1,7 @@
 
-WbSchemaReport 
--file=../../_DATA/$[sys_name]/content/sub_systems/$[subsys_name]/header/metadata.xml
--schemas=$[db_schema] 
+WbSchemaReport
+-file=../../../_DATA/$[sys_name]/content/sub_systems/$[subsys_name]/header/metadata.xml
+-schemas=$[db_schema]
 -types=SYNONYM,TABLE,VIEW
 -includeProcedures=true
 -includeTriggers=true
